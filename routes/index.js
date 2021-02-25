8c5e84ecaa41202470c24585b40692c036c2716a
Swann Pincet <swann.pincet@gmail.com>
2021-02-25 11:30:20 +0100

    added facial reco api from azure, set up route and test

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router()
 const uniqid = require('uniqid')
 const fs = require('fs')
 require('dotenv').config()
+const request = require('sync-request')
 
 const cloudinary = require('cloudinary').v2
 cloudinary.config({
@@ -19,19 +20,41 @@ router.post('/upload', async (req,res) => {
       return
     }
   })
-  try {
-    await cloudinary.uploader.upload(path, (error, response) => {
+ 
+    await cloudinary.uploader.upload(path, async (error, response) => {
       if (error) {
         fs.unlinkSync(path)
         res.json({result: false, error: "Issue uploading photo to cloudinary"})
       } else {
+        const params = {
+          returnFaceId: 'true',
+          returnFaceLandmarks: 'false',
+          returnFaceAttributes: 'age,gender,smile,facialHair,glasses,emotion,hair',
+         }
+        const options = {
+          qs: params,
+          body: JSON.stringify({url: response.secure_url}),
+          headers: {
+              'Content-Type': 'application/json',
+              'Ocp-Apim-Subscription-Key' : process.env.SUB_KEY
+          }
+         }
+        const rawData = await request('POST', process.env.URI_BASE, options)
+        const data = await JSON.parse(rawData.body)
+        
+        const {gender, age, facialHair, smile, hair} = data[0].faceAttributes
+        const faceData = {
+          gender,
+          age,
+          beard: facialHair.beard > 0 ? true : false,
+          hair : hair.hairColor[0].color,
+          smile: smile > 0 ? true : false
+        }
         fs.unlinkSync(path)
-        res.json({result: true, response})
+        res.json({result: true, response, faceData})
       }
     })
-  } catch (error) {
-    console.log(error)
-  }
+  
 })
 
 router.post('/upload-video', async (req,res) => {
